1407abce2f26e6699c5918bda39dfe9b09aa2d94
AntonRoston <rostonanton@gmail.com>
2024-04-15 15:08:29 +0300

    redact

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -2,30 +2,33 @@ import { useEffect, useState } from "react";
 import { ApiMovieRewiews } from "../ApiService/ApiService";
 import { useParams } from "react-router-dom";
 import css from "./MovieReviews.module.css";
+
 const MovieReviews = () => {
   const { movieId } = useParams();
-  const [rewiews, setRewiews] = useState([]);
+  const [reviews, setReviews] = useState([]);
+
   useEffect(() => {
     async function fetchData() {
       try {
         const results = await ApiMovieRewiews(movieId);
-        setRewiews(results);
+        setReviews(results);
       } catch (error) {
         console.log(error);
       }
     }
     fetchData();
-  }, [movieId, rewiews]);
+  }, [movieId]);
+
   return (
     <>
-      {rewiews.length === 0 ? (
+      {reviews.length === 0 ? (
         <p className={css.noResult}>
-          We do not have any rewiews for this movies😢
+          We do not have any reviews for this movie 😢
         </p>
       ) : (
-        <ul className={css.rewiewsList}>
-          {rewiews.results.map((item) => (
-            <li key={item.id} className={css.rewiewsItem}>
+        <ul className={css.reviewsList}>
+          {reviews.map((item) => (
+            <li key={item.id} className={css.reviewsItem}>
               <p>{item.author}</p>
               <p>{item.content}</p>
             </li>
